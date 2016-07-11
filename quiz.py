efd1b919abf3c53a4299751ba3216e820dd7116a
sharktamer <donohoe.shane@gmail.com>
2016-07-11 17:38:43 +0100

    Add more question attributes and test

--- quiz.py
+++ quiz.py
@@ -10,6 +10,15 @@ class Question():
         q_json = r.json()[0]
         self.question = q_json['question']
         self.answer = q_json['answer']
+        self.checked_answer = self.parse_answer(self.answer)
+        self.category = q_json['category']['title']
+        self.value = q_json['value']
+
+    def get_question(self):
+        return '{} ({}) [{}]'.format(self.question, self.category, self.value)
+
+    def parse_answer(self, answer):
+        return answer
 
 
 class Quiz():
@@ -18,4 +27,8 @@ class Quiz():
 if __name__ == "__main__":
     q = Question()
     print(q.question)
-    print(q.answer)
+    attempt = input('Answer: ')
+    if attempt.lower() == q.checked_answer.lower():
+        print('Correct! The answer was {}'.format(q.answer))
+    else:
+        print('Nope! The answer was {}'.format(q.answer))
