edc44fb2be42c651ca88fb7882b095765fce4d8c
sharktamer <donohoe.shane@gmail.com>
2016-07-12 11:09:56 +0100

    strip printed answer and checked answer separately

--- quiz.py
+++ quiz.py
@@ -2,24 +2,37 @@
 
 import requests
 # from sopel.modules import commands
+import re
 
 
 class Question():
     def __init__(self):
         r = requests.get('http://jservice.io/api/random')
         q_json = r.json()[0]
-        self.question = q_json['question']
-        self.answer = q_json['answer']
+        self.question = q_json['question'].strip()
+        self.answer = self.strip_answer(q_json['answer'])
         self.checked_answer = self.parse_answer(self.answer)
         self.category = q_json['category']['title']
         self.value = q_json['value']
 
     def get_question(self):
-        q, c, v = self.question.strip(), self.category, self.value
+        q, c, v = self.question, self.category, self.value
         return '{} ({}) [{}]'.format(q, c, v)
 
-    def parse_answer(self, answer):
+    def strip_answer(self, answer):
+        # strip any crap that should never be printed
+        # - html tags
+        answer = re.sub(r'\<.*?\>', '', answer)
         return answer
+
+    def parse_answer(self, answer):
+        # strip extraneous characters, making the question easier to answer
+        # - a, an and the from the beginning
+        answer = re.sub(r'^(the|a|an) |\<.*?\>', '', answer)
+        return answer.lower()
+
+    def attempt(self, attempt):
+        return (attempt is not None and self.checked_answer in attempt.lower())
 
 
 class Quiz():
@@ -29,7 +42,8 @@ if __name__ == "__main__":
     q = Question()
     print(q.question)
     attempt = input('Answer: ')
-    if attempt.lower() in q.checked_answer.lower():
+    # if attempt.lower() in q.checked_answer.lower():
+    if q.attempt(attempt):
         print('Correct! The answer was {}'.format(q.answer))
     else:
         print('Nope! The answer was {}'.format(q.answer))
