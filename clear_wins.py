e6cdee8486fa615690e80fedeec73288ed1d7fce
sharktamer <donohoe.shane@gmail.com>
2016-10-20 20:18:11 +0000

    add __name__ == "__main__" check to make sure script isnt run by sopel at startup (idiot)

--- clear_wins.py
+++ clear_wins.py
@@ -3,9 +3,10 @@
 import os
 import sqlite3
 
-dbfile = '{}/.sopel/default.db'.format(os.environ.get('HOME'))
-conn = sqlite3.connect(dbfile)
-c = conn.cursor()
-c.execute('DELETE FROM nick_values WHERE key = "quiz_wins"')
-conn.commit()
-conn.close()
+if __name__ == "__main__":
+    dbfile = '{}/.sopel/default.db'.format(os.environ.get('HOME'))
+    conn = sqlite3.connect(dbfile)
+    c = conn.cursor()
+    c.execute('DELETE FROM nick_values WHERE key = "quiz_wins"')
+    conn.commit()
+    conn.close()
